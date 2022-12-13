05dbd6339a048a2677ca7cef3cde909afd480f71
Janek <jan.krzysztof.wisniewski@gmail.com>
2022-12-13 21:36:02 +0100

    Swagger connected

---
+++ src/main/java/com/wisniewskijan/githubproxy/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.wisniewskijan.githubproxy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    //http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
---
+++ src/test/java/com/wisniewskijan/githubproxy/GitHubProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisniewskijan.githubproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
